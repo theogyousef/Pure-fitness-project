861929624301474ff1fb1b0bc33523f862379dc5
Mariamsamyy <mariamsamyeg@gmail.com>
2023-10-12 17:28:35 +0300

    Made some changes and fixes and error

--- PHP/Registeration.php
+++ PHP/Registeration.php
@@ -1,22 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
+<?php  
+    // include_once "../controller/dbh.inc.php";
+    // include_once "../controller/userFun.php";
 
+  ?>
 <head>
-  <meta charset="UTF-8" />
-  <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
-  <!-- <link rel="stylesheet" href="../CSS/registeration.css" /> -->
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="../CSS/registeration.css">
   <script src="https://kit.fontawesome.com/0b0cd0c012.js" crossorigin="anonymous"></script>
-
   <title>Registration</title>
-
   <style>
     <?php include "../CSS/registeration.css" ?>
   </style>
 </head>
-
 <body>
   <div class="containerbox">
     <!-- Nav menu -->
@@ -37,11 +37,10 @@
         <button class="btn white-btn" id="loginBtn" onclick="login()">Log in</button>
         <button class="btn" id="registerBtn" onclick="register()">Sign Up</button>
       </div>
-      <!-- responsive nav -->
+      <!-- Responsive nav -->
       <div class="nav-menu-btn">
         <i class="bx bx-menu" onclick="myMenuFunction()"></i>
       </div>
-
     </nav>
 
     <div class="form-box">
@@ -53,11 +52,11 @@
         </div>
         <form name="loginForm" id="loginForm" onsubmit="validationsLogin(event)">
           <div class="input-box">
-            <input type="email" class="input-field" placeholder="Email" name="email" />
+            <input type="email" class="input-field" placeholder="Email" name="email">
             <i class="bx bx-user"></i>
           </div>
           <div class="input-box">
-            <input id="id_password" type="password" class="input-field" placeholder="Password" name="pass" />
+            <input id="id_password" type="password" class="input-field" placeholder="Password" name="pass">
             <i class="bx bx-lock-alt"></i>
             <i id="togglePassword" class="fa-regular fa-eye-slash"></i>
             <div class="forgotpass">
@@ -65,85 +64,85 @@
             </div>
           </div>
           <div class="input-box">
-            <input type="submit" class="submit" value="Log in" style="margin-top: 15px;"/>
+            <input type="submit" class="submit" value="Log in" style="margin-top: 15px;">
           </div>
           <div style="height: 20px;">
             <span id="loginErrorMessages" style="display: none;"></span>
           </div>
-          
         </form>
-
       </div>
 
-
-      <form name="myform" id="myform" onsubmit="validations(event)">
-        <!-- register -->
+      <!-- Register -->
+      <form action="../controller/insert.php" method="post" name="myform" id="myform" onsubmit="validateRegistration(event)">
         <div class="register-container" id="register" style="height: 580px;">
           <div class="top">
-            <h3>Become a member ?</h3>
+            <h3>Become a member?</h3>
             <header>Register now</header>
           </div>
           <div class="two-forms">
             <div class="input-box">
-              <input type="text" class="input-field" placeholder="First name" name="fname" />
+              <input type="text" class="input-field" placeholder="First name" name="fname">
               <i class="bx bx-user"></i>
             </div>
             <div class="input-box">
-              <input type="text" class="input-field" placeholder="Last name" name="lname" />
+              <input type="text" class="input-field" placeholder="Last name" name="lname">
               <i class="bx bx-user"></i>
             </div>
           </div>
           <div class="input-box">
-            <input type="text" class="input-field" placeholder="Email" name="email" />
+            <input type="text" class="input-field" placeholder="Email" name="email">
             <i class="bx bx-envelope"></i>
           </div>
           <div class="input-box">
-            <input id="reg-password" type="password" class="input-field" placeholder="Password" name="password" />
+            <input id="reg-password" type="password" class="input-field" placeholder="Password" name="password">
             <i class="bx bx-lock-alt"></i>
             <i id="togglePassword2" class="fa-regular fa-eye-slash"></i>
           </div>
           <div class="content">
-            <div class="strength-box"> 
-            <p>Password must contain</p>
-            <ul class="requirement-list">
-    <li>
-      <i class="fa-solid fa-circle"></i>
-      <span>At least 8 Character length</span>
-    </li><li>
-      <i class="fa-solid fa-circle"></i>
-      <span>At least 1 number(0..9)</span>
-    </li><li>
-      <i class="fa-solid fa-circle"></i>
-      <span>At least 1 lowercase length (a..z)</span>
-    </li><li>
-      <i class="fa-solid fa-circle"></i>
-      <span>At least 1 Special symbol(@#$..)</span>
-    </li><li>
-      <i class="fa-solid fa-circle"></i>
-      <span>At least 1 uppercase letter(A..Z)</span>
-    </li>
-            </ul>
+            <div class="strength-box">
+              <p>Password must contain</p>
+              <ul class="requirement-list">
+                <li>
+                  <i class="fa-solid fa-circle"></i>
+                  <span>At least 8 characters in length</span>
+                </li>
+                <li>
+                  <i class="fa-solid fa-circle"></i>
+                  <span>At least 1 number (0-9)</span>
+                </li>
+                <li>
+                  <i class="fa-solid fa-circle"></i>
+                  <span>At least 1 lowercase letter (a-z)</span>
+                </li>
+                <li>
+                  <i class="fa-solid fa-circle"></i>
+                  <span>At least 1 special symbol (@#$..)</span>
+                </li>
+                <li>
+                  <i class="fa-solid fa-circle"></i>
+                  <span>At least 1 uppercase letter (A-Z)</span>
+                </li>
+              </ul>
             </div>
           </div>
           <div class="input-box">
-            <input id="conpassword" type="password" class="input-field" placeholder="confirm password"
-              name="confirmpassword" />
+            <input id="conpassword" type="password" class="input-field" placeholder="Confirm password" name="confirmpassword">
             <i class="bx bx-lock-alt"></i>
             <i id="togglePassword3" class="fa-regular fa-eye-slash"></i>
           </div>
-
           <!-- Add the span element for error messages -->
           <div style="height: 20px;">
             <span id="errorMessages" class="error-message"></span>
           </div>
           <div class="input-box">
-            <input type="submit" class="submit" value="Register" />
+            <input type="submit" class="submit" value="Register" id="submit-button">
+
           </div>
         </div>
       </form>
-
     </div>
   </div>
+  <script src="../JS/Registeration.js"></script>
 </body>
-      <script src="../JS/Registeration.js"> </script>
 </html>
+
