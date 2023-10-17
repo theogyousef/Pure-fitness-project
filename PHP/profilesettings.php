2afb0f2eb41af73cdbd0a1f2179c50f7344dfe81
joeehab <youssef2101042@miuegypt.edu.eg>
2023-10-17 17:00:52 +0300

    profile settings php

--- PHP/profilesettings.php
+++ PHP/profilesettings.php
@@ -1,3 +1,45 @@
+<script>
+  if (window.history.replaceState) {
+    window.history.replaceState(null, null, window.location.href);
+  }
+</script>
+
+<?php
+
+//require "../controller/config.php";
+require "../controller/profilesettingsfun.php";
+
+// Check for form submissions and perform the corresponding action
+if (isset($_POST["fileupload"])) {
+    uploadpic();
+} else if (isset($_POST["accountdetails"])) {
+    editdetails();
+} else if (isset($_POST["socialsave"])) {
+    updatesocials();
+} else if (isset($_POST["updatesecurity"])) {
+    updatepasswords();
+}
+
+
+if (!empty($_SESSION["id"])) {
+    $id = $_SESSION["id"];
+    $result = mysqli_query($conn, "SELECT * FROM users WHERE id = '$id'  ");
+    $row = mysqli_fetch_assoc($result);
+} else {
+    header("Location: registeration.php");
+}
+
+
+
+$instagramUsername = trim($row["instagram"]); // Remove leading/trailing whitespace
+$instagramURL = "https://www.instagram.com/" . rawurlencode($instagramUsername);
+
+$githubUsername = trim($row["github"]); // Remove leading/trailing whitespace
+$githubURL = "https://github.com/" . rawurlencode($githubUsername);
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,11 +50,11 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://use.fontawesome.com/3a2eaf6206.js"></script>
-
     <title>profile settings</title>
     <style>
-        <?php include "../CSS/prfilesettings.css" ?>
+        <?php include "../CSS/profilesettings.css" ?>
     </style>
+       <script src="../JS/profilesettings.js"></script>
 </head>
 
 <body>
@@ -20,10 +62,11 @@
 
         <!-- prfile settings nav bar -->
         <nav class="nav nav-borders">
-            <a class="nav-link active ms-0" id="detailsnav" href="#" onClick="detailsa()">Profile</a>
+            <a class="nav-link active ms-0" id="detailsnav" href="" onClick="detailsa()">Profile</a>
+            <a class="nav-link" href="#" id="socilanav" onClick="sociala()">Social</a>
             <a class="nav-link" href="#" id="securitysnav" onClick="securitya()">Security</a>
             <a class="nav-link" href="#" id="notificationsnav" onClick="notificationsa()">Notifications</a>
-            <a class="nav-link" href="home.php" >Home</a>
+            <a class="nav-link home" id="homenav" href="home.php">Home</a>
 
 
         </nav>
@@ -34,10 +77,12 @@
                 <div class="card">
                     <div class="card-body">
                         <div class="d-flex flex-column align-items-center text-center">
-                            <img src="../photos/yousef.jpg" alt="picture" class="rounded-circle p-1 bg-primary"
-                                width="110">
+                            <img src="<?php echo $row['profilepicture'] ?> " alt="picture"
+                                class="rounded-circle p-1 bg-primary" width="110">
                             <div class="mt-3">
-                                <h4>Yousef Ehab</h4>
+                                <h4>
+                                    <?php echo $row['firstname'] . " " . $row['lastname'] ?>
+                                </h4>
                                 <p class="text-secondary mb-1">Junior developer </p>
 
                             </div>
@@ -53,19 +98,12 @@
                                             d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22">
                                         </path>
                                     </svg>Github</h6>
-                                <a class="text-secondary" href="https://github.com/theogyousef">theogyousef</a>
+                                <a href="<?php echo $githubURL; ?>" class="text-secondary">
+                                    <?php echo $githubUsername; ?>
+                                </a>
+
                             </li>
-                            <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                                <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                        viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                        stroke-linecap="round" stroke-linejoin="round"
-                                        class="feather feather-twitter me-2 icon-inline text-info">
-                                        <path
-                                            d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z">
-                                        </path>
-                                    </svg>Twitter</h6>
-                                <span class="text-secondary">@joeehab</span>
-                            </li>
+
                             <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
                                 <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                                         viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
@@ -75,18 +113,11 @@
                                         <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
                                         <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
                                     </svg>Instagram</h6>
-                                <span class="text-secondary">@joeehabb</span>
+                                <a href="<?php echo $instagramURL; ?>" class="text-secondary">
+                                    <?php echo $instagramUsername; ?>
+                                </a>
                             </li>
-                            <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                                <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                        viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                        stroke-linecap="round" stroke-linejoin="round"
-                                        class="feather feather-facebook me-2 icon-inline text-primary">
-                                        <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z">
-                                        </path>
-                                    </svg>Facebook</h6>
-                                <span class="text-secondary">Yousef ehab</span>
-                            </li>
+
                         </ul>
                     </div>
                 </div>
@@ -99,87 +130,126 @@
                 <div class="card mb-4">
                     <div class="card-header">Account Details</div>
                     <div class="card-body">
-                        <form>
 
+
+                        <form method="post" enctype="multipart/form-data">
                             <div class="mb-3 photoselect">
-
-
                                 <label for="inputTag">
                                     Select Image <br />
                                     <i class="fa fa-2x fa-camera"></i>
-                                    <input id="inputTag" type="file"
+
+                                    <input id="inputTag" type="file" name="file"
                                         accept="image/png, image/jpg, image/gif, image/jpeg" />
                                     <br />
                                     <span id="imageName"></span>
                                 </label>
                             </div>
+                            <input name="fileupload" type="submit" class="btn btn-primary" value="Update photo">
+
+                        </form>
+
+
+                        <form method="post" id="detailss" name="details" onsubmit="validatedetails(event)">
                             <div class="mb-3">
                                 <label class="small mb-1" for="inputUsername">Username (how your name will appear to
                                     other users on the site)</label>
-                                <input class="form-control" id="inputUsername" type="text"
-                                    placeholder="Enter your username" value="joeehab">
+                                <input class="form-control" id="inputUsername" type="text" name="username"
+                                    placeholder="Enter your username" value="<?php echo $row['username'] ?>">
                             </div>
 
                             <div class="row gx-3 mb-3">
 
                                 <div class="col-md-6">
                                     <label class="small mb-1" for="inputFirstName">First name</label>
-                                    <input class="form-control" id="inputFirstName" type="text"
-                                        placeholder="Enter your first name" value="Yousef">
+                                    <input name="fname" class="form-control" id="inputFirstName" type="text"
+                                        placeholder="Enter your first name" value="<?php echo $row['firstname'] ?>">
                                 </div>
 
                                 <div class="col-md-6">
                                     <label class="small mb-1" for="inputLastName">Last name</label>
-                                    <input class="form-control" id="inputLastName" type="text"
-                                        placeholder="Enter your last name" value="Ehab">
+                                    <input name="lname" class="form-control" id="inputLastName" type="text"
+                                        placeholder="Enter your last name" value="<?php echo $row['lastname'] ?>">
                                 </div>
                             </div>
-
-
-
                             <div class="mb-3">
                                 <label class="small mb-1" for="inputEmailAddress">Email address</label>
-                                <input class="form-control" id="inputEmailAddress" type="email"
-                                    placeholder="Enter your email address" value="youssef2101042@miuegypt.edu.eg">
+                                <input name="email" class="form-control" id="inputEmailAddress" type="email"
+                                    placeholder="Enter your email address" value="<?php echo $row['email'] ?>">
                             </div>
 
 
-                            Save changes button
-                            <button class="btn btn-primary" type="button">Save changes</button>
+                            <div style="height: 20px;">
+                                <span id="DetailserrorMessages" style="display: none;"></span>
+                            </div>
+                            <br>
+                            <input name="accountdetails" type="submit" class="btn btn-primary" value="Save changes">
                         </form>
                     </div>
                 </div>
             </div>
 
-            <!-- Security -->
-            <div class="col-xl-8" style="display: none;" id="security">
+           
+            <!-- Social -->
+            <div class="col-xl-8" style="display: none;" id="social">
+                <div class="card mb-4">
+                    <div class="card-header">Social</div>
+                    <div class="card-body">
+                        <form method="post" id="socialss" name="socialss">
+
+                            <div class="mb-3">
+                                <label class="small mb-1" for="github">Github :</label>
+                                <input class="form-control" name="github" id="github" type="text"
+                                    value="<?php echo $row['github'] ?>">
+                            </div>
+
+                            <div class="mb-3">
+                                <label class="small mb-1" for="instagram">Instagram :</label>
+                                <input class="form-control" name="instagram" id="instagram" type="text"
+                                    value="<?php echo $row['instagram'] ?> ">
+                            </div>
+
+
+
+                            <!-- // Save changes button -->
+                            <button name="socialsave" class="btn btn-primary" type="submit">Update my socials</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+             <!-- Security -->
+             <div class="col-xl-8" style="display: none;" id="security">
                 <div class="card mb-4">
                     <div class="card-header">Security</div>
                     <div class="card-body">
-                        <form>
+                        <form method="post" id="security" name="security" onsubmit="validatesecurity(event)">
 
                             <div class="mb-3">
                                 <label class="small mb-1" for="oldpassword">Old passowrd</label>
-                                <input class="form-control" id="oldpassword" type="password">
+                                <input name="oldpassword" class="form-control" id="oldpassword" type="password">
                             </div>
 
                             <div class="mb-3">
                                 <label class="small mb-1" for="newpassword">New passowrd</label>
-                                <input class="form-control" id="newpassword" type="password">
+                                <input name="newpassword" class="form-control" id="newpassword" type="password">
                             </div>
 
                             <div class="mb-3">
                                 <label class="small mb-1" for="conpassword">Confirm new passowrd</label>
-                                <input class="form-control" id="conpassword" type="password">
+                                <input name="conpassword" class="form-control" id="conpassword" type="password">
                             </div>
-
+                            <div style="height: 20px;">
+                                <span id="SecurityerrorMessages" style="display: none;"></span>
+                            </div><br>
 
                             <!-- // Save changes button -->
-                            <button class="btn btn-primary" type="button">Update password</button>
+                            <input name="updatesecurity" type="submit" class="btn btn-primary" value="Update password">
+
                         </form>
                     </div>
                 </div>
             </div>
+
 
             <!-- Notifications -->
             <div class="col-xl-8" style="display: none;" id="notifications">
@@ -209,52 +279,74 @@
         </div>
     </div>
 
-    <script>
-        var detailsnav = document.getElementById("detailsnav");
-        var securitysnav = document.getElementById("securitysnav");
-        var notificationsnav = document.getElementById("notificationsnav");
+ 
+<script> 
 
-        var details = document.getElementById("details");
-        var security = document.getElementById("security");
-        var notifications = document.getElementById("notifications")
+var detailsnav = document.getElementById("detailsnav");
+  var securitysnav = document.getElementById("securitysnav");
+  var socilanav = document.getElementById("socilanav");
+  var notificationsnav = document.getElementById("notificationsnav");
 
-        function detailsa() {
-            detailsnav.classList.add("active");
-            securitysnav.classList.remove("active");
-            notificationsnav.classList.remove("active");
-
-
-            details.style.display = "block";
-            security.style.display = "none";
-            notifications.style.display = "none";
-        }
-
-        function securitya() {
-            detailsnav.classList.remove("active");
-            securitysnav.classList.add("active");
-            notificationsnav.classList.remove("active");
-
-            details.style.display = "none";
-            security.style.display = "block";
-            notifications.style.display = "none";
-
-        }
-
-        function notificationsa() {
-
-            detailsnav.classList.remove("active");
-            securitysnav.classList.remove("active");
-            notificationsnav.classList.add("active");
-
-            details.style.display = "none";
-            security.style.display = "none";
-            notifications.style.display = "block";
-
-        }
+  var details = document.getElementById("details");
+  var security = document.getElementById("security");
+  var social = document.getElementById("social");
+  var notifications = document.getElementById("notifications");
 
 
-    </script>
+  function detailsa() {
+      detailsnav.classList.add("active");
+      securitysnav.classList.remove("active");
+      socilanav.classList.remove("active");
+      notificationsnav.classList.remove("active");
 
+
+      details.style.display = "block";
+      security.style.display = "none";
+      social.style.display = "none";
+      notifications.style.display = "none";
+  }
+
+  function securitya() {
+      detailsnav.classList.remove("active");
+      securitysnav.classList.add("active");
+      socilanav.classList.remove("active");
+      notificationsnav.classList.remove("active");
+
+      details.style.display = "none";
+      security.style.display = "block";
+      social.style.display = "none";
+      notifications.style.display = "none";
+
+  }
+
+  function sociala() {
+
+      detailsnav.classList.remove("active");
+      securitysnav.classList.remove("active");
+      socilanav.classList.add("active");
+      notificationsnav.classList.remove("active");
+
+      details.style.display = "none";
+      security.style.display = "none";
+      social.style.display = "block";
+      notifications.style.display = "none";
+
+  }
+
+  function notificationsa() {
+
+      detailsnav.classList.remove("active");
+      securitysnav.classList.remove("active");
+      socilanav.classList.remove("active");
+      notificationsnav.classList.add("active");
+
+      details.style.display = "none";
+      security.style.display = "none";
+      social.style.display = "none";
+      notifications.style.display = "block";
+
+  }
+  </script>
 </body>
 
 </html>
