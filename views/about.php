c3ead623a11880d2f4adc2f1b1b82bd887589f87
moazzmuhammed <moazzmuhammed123@gmail.com>
2023-10-24 12:50:39 +0300

    modifying about us page

--- views/about.php
+++ views/about.php
@@ -14,33 +14,37 @@
   <title>About Us</title>
   <style>
     <?php include "../public/CSS/about.css";
-    
     include "header.php"
      ?> 
 
   </style>
   <body>
   <header>
-    <h1>About Us</h1>
-  </header>
-  <section id="introduction">
-    <h2>Welcome to Pure Fitness</h2>
-    <p>We are passionate about fitness and providing high-quality gym equipment to help you achieve your fitness goals. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-  </section>
-  <section id="history">
-    <h2>Our Journey</h2>
-    <p>Since our establishment in 2010, Pure Fitness has been dedicated to delivering top-notch gym equipment and exceptional customer service. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-  </section>
-  <section id="goals">
-    <h2>Our Goals</h2>
-    <ul>
-      <li>Provide a wide range of high-quality gym equipment to meet the diverse needs of our customers.</li>
-      <li>Offer competitive prices without compromising on the quality and durability of our products.</li>
-      <li>Deliver exceptional customer service, ensuring prompt responses to inquiries and efficient after-sales support.</li>
-      <li>Continuously innovate and introduce new fitness products to keep up with the latest industry trends.</li>
-      <li>Promote a healthy and active lifestyle by encouraging our customers to achieve their fitness goals.</li>
-    </ul>
-  </section>
+        <h1>About Us</h1>
+    </header>
+
+    <div class="container">
+        <section id="introduction" class="box">
+            <h2>Welcome to Pure Fitness</h2>
+            <p>We are passionate about fitness and providing high-quality gym equipment to help you achieve your fitness goals. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </section>
+
+        <section id="history" class="box">
+            <h2>Our Journey</h2>
+            <p>Since our establishment in 2010, Pure Fitness has been dedicated to delivering top-notch gym equipment and exceptional customer service. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </section>
+
+        <section id="goals" class="box">
+            <h2>Our Goals</h2>
+            <ul>
+                <li>Provide a wide range of high-quality gym equipment to meet the diverse needs of our customers.</li>
+                <li>Offer competitive prices without compromising on the quality and durability of our products.</li>
+                <li>Deliver exceptional customer service, ensuring prompt responses to inquiries and efficient after-sales support.</li>
+                <li>Continuously innovate and introduce new fitness products to keep up with the latest industry trends.</li>
+                <li>Promote a healthy and active lifestyle by encouraging our customers to achieve their fitness goals.</li>
+            </ul>
+        </section>
+    </div>
 
 <footer>
     <?php 
