14cab95157fc37793ce3172609979cf076ac6d5b
Mariamsamyy <mariamsamyeg@gmail.com>
2023-11-14 15:32:32 +0200

    added the styling and the js of the slick slider

--- public/JS/index.js
+++ public/JS/index.js
@@ -97,3 +97,32 @@ nextButton.addEventListener('click', () => {
 });
 
 showReview(currentIndex);
+
+$(document).ready(function() {
+  $(".slider").slick({
+      arrows: true, // Keep arrows enabled
+      dots: false,
+      slidesToShow: 3,
+      autoplay: false,
+      speed: 900,
+      autoplaySpeed: 700,
+      responsive: [
+          {
+              breakpoint: 768,
+              settings: {
+                  slidesToShow: 2
+              }
+          },
+          {
+              breakpoint: 550,
+              settings: {
+                  slidesToShow: 1
+              }
+          }
+      ]
+  });
+
+  // Add custom arrow icons
+  $(".slick-next").html('<i class="fa-solid fa-arrow-right"></i>');
+  $(".slick-prev").html('<i class="fas fa-chevron-left"></i>');
+});
