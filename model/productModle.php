b6d7732ee1a8c1b4f6ea0727ca4cdf682e9e3664
Mariamsamyy <mariamsamyeg@gmail.com>
2023-11-22 10:15:00 +0200

    added the product model

--- model/productModle.php
+++ model/productModle.php
@@ -1,7 +1,14 @@
 <?php
 
 include "../controller/config.php";
-class ProductModle{
+class ProductModle
+{
 
-  
+    public static function allproducts()
+    {
+        include "../controller/config.php";
+        $query =  "SELECT * FROM products";
+         $result= mysqli_query($conn, $query);
+         return $result;
+    }
 }
