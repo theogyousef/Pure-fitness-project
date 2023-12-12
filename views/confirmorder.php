e6a3a6f0a5f31afafdfb8232231348c23ebe2c30
moazzmuhammed <moazzmuhammed123@gmail.com>
2023-12-13 00:59:36 +0200

    Update confirmorder.php

--- views/confirmorder.php
+++ views/confirmorder.php
@@ -22,30 +22,31 @@ if (isset($_POST["confirm"])) {
 	require '../controller/config.php';
 	$order_id = rand(1000, 9999);
 	echo "order id " . $order_id . " <br>";
-	$user_id = $id ;
+	$user_id = $id;
 	echo "user id = " . $user_id;
 	//insert into orders 
-	mysqli_query($conn,"INSERT into orders (order_id , user_id ) VALUES ('$order_id' , '$user_id')");
+	mysqli_query($conn, "INSERT into orders (order_id , user_id ) VALUES ('$order_id' , '$user_id')");
 	$total = $_SESSION['total'];
 	echo "Total from session: " . $total . "<br>";
 	//insert into orders_details 
-	mysqli_query($conn,"INSERT into orders_details (order_id , status , total ) VALUES ('$order_id' , 'pending' , '$total' )");
-	$totalquantity = 0 ;
-	foreach ($_SESSION['products'] as $product):
+	mysqli_query($conn, "INSERT into orders_details (order_id , status , total ) VALUES ('$order_id' , 'pending' , '$total' )");
+	$totalquantity = 0;
+	foreach ($_SESSION['products'] as $product) :
 		$totalquantity += $product['quantity'];
 		echo $product['id'] . " " . $product['quantity'] . "<br>";
 		$product_id = $product['id'];
 		$quantity = $product['quantity'];
 		echo "product id " . $product_id . "and quantity = " . $quantity . "<br>";
 		//insert into orders_product_details 
-		mysqli_query($conn,"INSERT into  order_product_details  (order_id , product_id , quantity ) VALUES ('$order_id' , '$product_id' , '$quantity' )");
+		mysqli_query($conn, "INSERT into  order_product_details  (order_id , product_id , quantity ) VALUES ('$order_id' , '$product_id' , '$quantity' )");
 
 	endforeach;
 	echo $totalquantity;
 
 
-echo "                    success                ";
-header("Location: orders");
+	echo "success";
+	$_SESSION['products'] = array();
+	header("Location: orders");
 }
 include 'header.php';
 ?>
@@ -60,8 +61,7 @@ include 'header.php';
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/js/all.min.js"></script>
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
-	<link rel="stylesheet"
-		href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
 	<link rel="stylesheet" href="../public/CSS/wishlist.css">
 	<link rel="stylesheet" href="../public/CSS/cart_display.css">
