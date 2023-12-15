dce41a977717a973cb82e5cf3657b275419a76f9
moazzmuhammed <moazzmuhammed123@gmail.com>
2023-12-16 01:29:47 +0200

    updating cart quantity and total itslelf

--- public/JS/cart_display.js
+++ public/JS/cart_display.js
@@ -1,51 +1,53 @@
-src = "https://code.jquery.com/jquery-3.6.0.min.js"
-
 document.addEventListener('DOMContentLoaded', function () {
-
-    document.querySelector('.update-cart-button').addEventListener('click', function () {
-
-        document.querySelectorAll('.quantity-selector').forEach(selector => {
-            const quantityInput = selector.querySelector('.quantity-input');
-            const productId = quantityInput.getAttribute('data-product-id');
-
-            let newQuantity = parseInt(quantityInput.value, 10);
-            if (newQuantity <= 0) {
-                alert('Quantity must be greater than zero.');
-                return;
-            }
-
-            quantityInput.value = newQuantity;
-
-            const priceElement = selector.closest('tr').querySelector('.price');
-            const unitPrice = parseFloat(priceElement.getAttribute('data-unit-price'));
-            const totalPrice = newQuantity * unitPrice;
-            priceElement.textContent = totalPrice.toFixed(2) + ' EGP';
-
-            const form = document.querySelector('form');
-            const formQuantityInput = form.querySelector(`[name="quantity[${productId}]"]`);
-            formQuantityInput.value = newQuantity;
-        });
-
-        document.querySelector('form').submit();
-    });
-
     document.querySelectorAll('.quantity-selector').forEach(selector => {
         const decrementButton = selector.querySelector('.decrement');
         const incrementButton = selector.querySelector('.increment');
         const quantityInput = selector.querySelector('.quantity-input');
+        const productId = quantityInput.getAttribute('data-product-id');
+        console.log('Product ID:', productId);
 
         decrementButton.addEventListener('click', () => {
             let quantity = parseInt(quantityInput.value, 10);
             if (quantity > 1) {
                 quantity--;
-                quantityInput.value = quantity;
+                updateQuantity(productId, quantity);
             }
         });
 
         incrementButton.addEventListener('click', () => {
             let quantity = parseInt(quantityInput.value, 10);
             quantity++;
-            quantityInput.value = quantity;
+            updateQuantity(productId, quantity);
         });
     });
+
+    function updateQuantity(productId, newQuantity) {
+        // Use AJAX to update the server-side quantity
+        const formData = new FormData();
+        formData.append('updatecart', true);
+        formData.append('product_id', productId);
+        formData.append('quantity', newQuantity);
+
+        fetch('update_cart.php', {
+            method: 'POST',
+            body: formData
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data.status === 'success') {
+                    // Optionally handle success, e.g., update total price
+                    console.log('Quantity updated successfully');
+
+                    // Reload the page after successful update
+                    window.location.reload();
+                } else {
+                    console.error('Failed to update quantity:', data.message);
+                }
+            })
+            .catch(error => {
+                console.error('Error updating quantity:', error);
+            });
+    }
+
 });
+
