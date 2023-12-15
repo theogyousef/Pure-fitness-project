8da5cab6ae8688ca6f945a8dfed17bec87fd4123
joeehab <youssef2101042@miuegypt.edu.eg>
2023-12-15 16:26:01 +0200

    user search in admin dasboard fix

--- controller/searchuser.php
+++ controller/searchuser.php
@@ -11,21 +11,31 @@ $users = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
 //<<<< returning again the html after searching >>>>
 if (isset($_GET['html']) && $_GET['html'] == 'true') {
-    foreach ($users as $user) {
-        if ($user["guest"] != '1') {
-            echo "<tr>
-                    <td>{$user['id']}</td>
-                    <td>{$user['firstname']} {$user['lastname']}</td>
-                    <td>{$user['email']}</td>
-                    <td>" . ($user['admin'] ? 'Admin' : '') . "</td>
-                    <td>" . ($user['deactivated'] ? 'Deactivated' : '') . "</td>
-                    <td><a href='edituser?id={$user['id']}' style='color: orange;'><span class='fas fa-edit'></span></a></td>
-                    <td><a href='deleteuser?id={$user['id']}' style='color: red;'><span class='fas fa-trash-alt'></span></a></td>
-                    <td><a href='makeuser?id={$user['id']}' style='color: green;'><span class='fas fa-user'></span></a></td>
-                    <td><a href='makeadmin?id={$user['id']}' style='color: black;'><span class='fas fa-user-shield'></span></a></td>
-                  </tr>";
-        }
+  // ... (previous code)
+
+foreach ($users as $user) {
+    $id = $user['id'];
+    $query2 = "SELECT * from permissions where user_id = '$id'";
+    $result2 = mysqli_query($conn, $query2);
+    $permissions = mysqli_fetch_assoc($result2); // Use mysqli_fetch_assoc instead of mysqli_fetch_all
+
+    if ($permissions && isset($permissions["guest"])) {
+        echo "<tr>
+                <td>{$user['id']}</td>
+                <td>{$user['firstname']} {$user['lastname']}</td>
+                <td>{$user['email']}</td>
+                <td>" . ($permissions['admin'] ? 'Admin' : '') . "</td>
+                <td>" . ($permissions['deactivated'] ? 'Deactivated' : '') . "</td>
+                <td><a href='edituser?id={$user['id']}' style='color: orange;'><span class='fas fa-edit'></span></a></td>
+                <td><a href='deleteuser?id={$user['id']}' style='color: red;'><span class='fas fa-trash-alt'></span></a></td>
+                <td><a href='makeuser?id={$user['id']}' style='color: green;'><span class='fas fa-user'></span></a></td>
+                <td><a href='makeadmin?id={$user['id']}' style='color: black;'><span class='fas fa-user-shield'></span></a></td>
+              </tr>";
     }
+}
+
+// ... (remaining code)
+
 } else {
     echo json_encode($users);
 }
